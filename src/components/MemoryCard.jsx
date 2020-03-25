54caad188e61e0857fc0b3e0187e71bd942514fe
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-25 10:43:33 -0400

    Added front memory card and repositioned cards into nested divs

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -5,9 +5,17 @@ class MemoryCard extends Component {
     render() {
       return (
         <div className="header">
-        <div className="MemoryCard">
-        <img src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png'/>
-        </div>
+          <div className="MemoryCard">
+            <div className="MemoryCardInner">
+              <div className="MemoryCardFront">
+                ∆
+              </div>              
+              <div className="MemoryCardBack">
+              <img src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png'/>
+              </div>
+
+            </div>
+          </div>
         </div>
       );
     }
