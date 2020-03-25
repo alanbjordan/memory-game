3a4c1304f87cf78b36836b48361bff1589a716ef
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-24 22:27:57 -0400

    Created MemoryCard Component in a .jsx file

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import './MemoryCard.css'
+
+class MemoryCard extends Component {
+    render() {
+      return (
+        <div className="header">
+        <div className="MemoryCard">
+        <img src='https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png'/>
+        </div>
+        </div>
+      );
+    }
+  }
+
+export default MemoryCard;
